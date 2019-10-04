6120aae1aad10de3297106ac6c848ded7fc7e4a0
Helga Pavluchenko <olga@pavluchenko.com>
2019-10-04 22:06:21 +0300

    initial commit

---
+++ SoundControlMac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SoundControlMac
+//
+//  Created by Helga on 10/4/19.
+//  Copyright © 2019 Olga Pavluchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
